308a1e20dfe9c8e8a055bffd8111091469bab549
Krishna455424 <dvranjitha55@gmail.com>
2026-05-04 18:38:44 +0000

    save

--- main.tf
+++ main.tf
@@ -13,3 +13,22 @@ resource "signalfx_time_chart" "memory" {
     A = data("memory.utilization").publish(label="Memory")
   EOF
 }
+
+resource "signalfx_detector" "cpu_high" {
+  name = "High CPU Utilization Detector"
+
+  program_text = <<-EOF
+    A = data("cpu.utilization").publish(label="CPU")
+    detect(when(A > 0.5)).publish("High CPU")
+  EOF
+
+  rule {
+    description = "CPU usage is above 50%"
+    severity    = "Major"
+    detect_label = "High CPU"
+
+    notifications = [
+      "Email,hssrikrishna39@gmail.com"
+    ]
+  }
+}
